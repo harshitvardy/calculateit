801361f3299466005dc3fd4fad3898ca3ad30551
harshitvardy <harshitvardy2003@gmail.com>
2017-03-17 13:36:08 +0530

    Add files via upload

---
+++ doit.py
@@ -0,0 +1,39 @@
+def add(x, y):
+	return x+y
+def subtract(x, y):
+	return x-y
+def multiply(x, y):
+	return x*y
+def devide(x, y):
+	return x/y
+
+print "Select operation."
+print ""
+print "1.Add"
+print "2.Subtract"
+print "3.Multiply"
+print "4.Divide"
+print "" 
+
+choice = raw_input("Enter choice(1/2/3/4): ")
+print ""
+
+num1 = int(raw_input("Enter first number: "))
+print ""
+num2 = int(raw_input("Enter second number: "))
+print "" 
+
+if choice == '1':
+	print num1,"+",num2,"=", add(num1, num2)
+
+elif choice =='2':
+	print num1,"-",num2,"=", subtract(num1, num2)
+
+elif choice =='3':
+	print num1,"*",num2,"=", multiply(num1, num2)
+
+elif chioce =='4':
+	print num1,"/",num2,"=", divide(num1, num2)
+
+else:
+	print "Invalid Input"
